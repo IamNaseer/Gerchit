09e64d3d67aa430657f5814fcc79123c9eb9adc0
ABDUL NASEER <naseersonu3@gmail.com>
2022-08-03 07:44:28 +0530

    Updates

---
+++ precache-manifest.58ab180713804659313b400bfc015de4.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c136448d4a3e4fd0c72",
+    "url": "/Gerchit/static/css/main.0e8bb9d7.chunk.css"
+  },
+  {
+    "revision": "7c136448d4a3e4fd0c72",
+    "url": "/Gerchit/static/js/main.7c136448.chunk.js"
+  },
+  {
+    "revision": "d73d40f797148ebdb451",
+    "url": "/Gerchit/static/js/1.d73d40f7.chunk.js"
+  },
+  {
+    "revision": "d4aa0417ce394d99a2a0",
+    "url": "/Gerchit/static/js/runtime~main.d4aa0417.js"
+  },
+  {
+    "revision": "be2151f4e9af529f67ce80896ed8374a",
+    "url": "/Gerchit/static/media/meal.be2151f4.mp4"
+  },
+  {
+    "revision": "d719065bb2726afe77e90cec8ea81d5a",
+    "url": "/Gerchit/static/media/bg.d719065b.png"
+  },
+  {
+    "revision": "85d27f24ca6480fa5e10c95311f10e19",
+    "url": "/Gerchit/static/media/chef.85d27f24.png"
+  },
+  {
+    "revision": "c7fdecee851896549a8f719d1d792e75",
+    "url": "/Gerchit/static/media/G.c7fdecee.png"
+  },
+  {
+    "revision": "5a3891e8d95239f82c09715ae11941b6",
+    "url": "/Gerchit/static/media/gallery01.5a3891e8.png"
+  },
+  {
+    "revision": "2dd1c3366e4ba309e1ab5ef1c4d31f2d",
+    "url": "/Gerchit/static/media/gallery02.2dd1c336.png"
+  },
+  {
+    "revision": "3706b6f7f3577e20cbc4bec6e7698eb5",
+    "url": "/Gerchit/static/media/gallery03.3706b6f7.png"
+  },
+  {
+    "revision": "f51d9759a0055fffba51cb9949949771",
+    "url": "/Gerchit/static/media/gallery04.f51d9759.png"
+  },
+  {
+    "revision": "e96f9bd45b8e0b34eeb03e93f547ccf8",
+    "url": "/Gerchit/static/media/knife.e96f9bd4.png"
+  },
+  {
+    "revision": "5c00c519489e1238ee18cc42a47f141a",
+    "url": "/Gerchit/static/media/logo.5c00c519.png"
+  },
+  {
+    "revision": "cc54616224f2b073f9400318c172d8a6",
+    "url": "/Gerchit/static/media/menu.cc546162.png"
+  },
+  {
+    "revision": "179769474d7506d29c78a6bec834b00c",
+    "url": "/Gerchit/static/media/overlaybg.17976947.png"
+  },
+  {
+    "revision": "72422a1acc4f48e3d82ae3bfa6d1b9d8",
+    "url": "/Gerchit/static/media/spoon.72422a1a.svg"
+  },
+  {
+    "revision": "fd16528f71a845cc74618cf2bd3d8995",
+    "url": "/Gerchit/static/media/welcome.fd16528f.png"
+  },
+  {
+    "revision": "03ef4140ada413449ce1d79046217392",
+    "url": "/Gerchit/static/media/findus.03ef4140.png"
+  },
+  {
+    "revision": "c2d7d58a629c456aac6406b8986272fb",
+    "url": "/Gerchit/static/media/laurels.c2d7d58a.png"
+  },
+  {
+    "revision": "a807ceab88095e0cbece581d1600f52d",
+    "url": "/Gerchit/static/media/award01.a807ceab.png"
+  },
+  {
+    "revision": "ff49ca57a8d44a4596c62d4966eb5788",
+    "url": "/Gerchit/static/media/award02.ff49ca57.png"
+  },
+  {
+    "revision": "73a36a9eac74bab796a0e1c799a7ad4a",
+    "url": "/Gerchit/static/media/award03.73a36a9e.png"
+  },
+  {
+    "revision": "d13582bfb80458cf3abd2268d5b5b581",
+    "url": "/Gerchit/static/media/award05.d13582bf.png"
+  },
+  {
+    "revision": "f1363e1deeb3ee759b3a365913a97c85",
+    "url": "/Gerchit/static/media/sign.f1363e1d.png"
+  },
+  {
+    "revision": "5a646916ee293c50576905b609874f13",
+    "url": "/Gerchit/index.html"
+  }
+];
